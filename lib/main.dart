c5c22e70303a60e860816d9b4263437108545074
Shubham <shubhamsheliya9825@gmail.com>
2024-04-18 18:19:19 -0400

    Design candidate screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Menu',
+      title: 'Connect Tech',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
