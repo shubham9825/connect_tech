f37d6d7c3c4b63d66cad00233fd9572139e96b08
Jai Maurya <jaimaurya123456@gmail.com>
2024-04-19 17:57:11 -0400

    The Form for posting a Job

---
+++ lib/screens/create_post_screen.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+
+class CreatePostScreen extends StatefulWidget {
+  @override
+  _CreatePostScreenState createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _companyController = TextEditingController();
+  final TextEditingController _salaryController = TextEditingController();
+  final TextEditingController _requirementsController = TextEditingController();
+
+  bool _isJobListing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Post'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                maxLines: 3,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _locationController,
+                decoration: InputDecoration(
+                  labelText: 'Location',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _companyController,
+                decoration: InputDecoration(
+                  labelText: 'Company',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _salaryController,
+                decoration: InputDecoration(
+                  labelText: 'Salary in CAD',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _requirementsController,
+                decoration: InputDecoration(
+                  labelText: 'Requirements',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                maxLines: 3,
+              ),
+              SizedBox(height: 20),
+              CheckboxListTile(
+                title: Text('Job Listing'),
+                value: _isJobListing,
+                onChanged: (value) {
+                  setState(() {
+                    _isJobListing = value!;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle form submission
+                    _submitForm();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _submitForm() {
+    // Retrieve form values
+    String title = _titleController.text;
+    String description = _descriptionController.text;
+    String location = _locationController.text;
+    String company = _companyController.text;
+    String salary = _salaryController.text;
+    String requirements = _requirementsController.text;
+
+    // Here we take care of the form validation
+    if (title.isEmpty ||
+        description.isEmpty ||
+        location.isEmpty ||
+        company.isEmpty ||
+        salary.isEmpty ||
+        requirements.isEmpty) {
+      // If user does not fill data then show error message
+      _showErrorDialog('Please fill in all fields.');
+      return;
+    }
+
+    // Showing confirmation message
+    _showConfirmationDialog('Your Job opening will be visible soon');
+
+    // Clearing the form fields
+    _titleController.clear();
+    _descriptionController.clear();
+    _locationController.clear();
+    _companyController.clear();
+    _salaryController.clear();
+    _requirementsController.clear();
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showConfirmationDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Success'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _locationController.dispose();
+    _companyController.dispose();
+    _salaryController.dispose();
+    _requirementsController.dispose();
+    super.dispose();
+  }
+}
